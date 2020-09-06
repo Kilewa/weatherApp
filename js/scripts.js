9cfdd18bbd570094ba8f7bd7629bd5e24d4aca83
kilewa <kilewageorge230@gmail.com>
2020-09-06 13:34:47 +0300

    added styles and Api key

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+const api = {
+    key: "0efccec1f8bb677360d694770d9149a1",
+    baseurl: "https://api.openweathermap.org/data/2.5/"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const btn = document.querySelector('.btn');
+// const inputValue = document.querySelector('.formInput');
+// const city = document.querySelector('.name');
+// const description = document.querySelector('.desc');
+// const temperature = document.querySelector('.temp');
+
+
+// btn.addEventListener('click',function(){
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q=' +inputValue.value+ '&appid=0efccec1f8bb677360d694770d9149a1')
+
+//     .then(response => response.json())
+//     .then(data => {
+//         const cityValue = data['name'];
+//         const temperatureValue = data['main']['temp'];
+//         const descriptionValue = data['weather'][0]['description'];
+
+//         city.innerHTML = cityValue;
+//         temperature.innerHTML = temperatureValue;
+//         description.innerHTML = descriptionValue;
+//     })
+// .catch( err => alert("Wrong city name!")) 
+
+// })
+
