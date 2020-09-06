4792748b308a6b130f6d026cbfc7ab82c10d8fce
kilewa <kilewageorge230@gmail.com>
2020-09-06 17:19:26 +0300

    finished ui and fetch

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,53 @@ const api = {
     baseurl: "https://api.openweathermap.org/data/2.5/"
 }
 
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(e){
+    if (e.keyCode == 13){
+        getResults(searchbox.value)
+    }
+}
+function getResults (query) {
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
+}
+
+function displayResults (weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBulder(now)
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>&#176C</span>`
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+    let hiLow = document.querySelector('.hi-low');
+    hiLow.innerText = `${weather.main.temp_min}  °C / ${weather.main.temp_max}  °C`
+}
+
+function dateBulder (d) {
+    let months =  ["January", "February", "March", "April", "May", "June", "July", 
+                    "August", "September", "October", "November", "December"];
+    let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+
+}
+
 
 
 
